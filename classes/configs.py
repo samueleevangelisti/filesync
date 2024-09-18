c7ec033783678f62eca7d6699bf9a0639f2fa637
Samuele Evangelisti <samueleevangelisti@yahoo.it>
2024-09-18 23:04:52 +0200

    .

--- classes/configs.py
+++ classes/configs.py
@@ -14,7 +14,7 @@ class Configs:
 
 
 
-    def __init__(self, order, is_source_ftp, source_path, is_destination_ftp, destination_path, is_delete, is_dual, ignore_regex_list, rename_dict):
+    def __init__(self, order, is_source_ftp, source_path, is_destination_ftp, destination_path, is_force, is_delete, is_dual, ignore_regex_list, rename_dict):
         '''
         Parameters
         ----------
@@ -28,6 +28,8 @@ class Configs:
             True if destination is ftp
         destination_path : str
             Destination path
+        is_force : bool
+            True to force copy of files from source to destination
         is_delete : bool
             True to delete file not in source
         is_dual : bool
@@ -42,6 +44,7 @@ class Configs:
         typechecks.check(source_path, str)
         typechecks.check(is_destination_ftp, bool)
         typechecks.check(destination_path, str)
+        typechecks.check(is_force, bool)
         typechecks.check(is_delete, bool)
         typechecks.check(is_dual, bool)
         typechecks.check(ignore_regex_list, list)
@@ -51,6 +54,7 @@ class Configs:
         self.source_path = source_path
         self.is_destination_ftp = is_destination_ftp
         self.destination_path = destination_path
+        self.is_force = is_force
         self.is_delete = is_delete
         self.is_dual = is_dual
         self.folder_ignore_regex_list = []
@@ -79,7 +83,7 @@ class Configs:
         -------
         Configs
         '''
-        return Configs(click.prompt('order', type=int, default=0, show_default=True), click.prompt('is_source_ftp', type=bool, default=False, show_default=True), click.prompt('source_path', type=str, default='.', show_default=True), click.prompt('is_destination_ftp', type=bool, default=False, show_default=True), click.prompt('destination_path', type=str, default='.', show_default=True), click.prompt('is_delete', type=bool, default=True, show_default=True), click.prompt('is_dual', type=bool, default=False, show_default=True), click.prompt('ignore_regex_list', type=list, default=[
+        return Configs(click.prompt('order', type=int, default=0, show_default=True), click.prompt('is_source_ftp', type=bool, default=False, show_default=True), click.prompt('source_path', type=str, default='.', show_default=True), click.prompt('is_destination_ftp', type=bool, default=False, show_default=True), click.prompt('destination_path', type=str, default='.', show_default=True), click.prompt('is_force', type=bool, default=False, show_default=True), click.prompt('is_delete', type=bool, default=True, show_default=True), click.prompt('is_dual', type=bool, default=False, show_default=True), click.prompt('ignore_regex_list', type=list, default=[
             '.*\\.git/',
             '.*\\_\\_pycache\\_\\_/',
             '.*filesync\\-configs\\.json',
@@ -99,6 +103,7 @@ class Configs:
         self.source_path = click.prompt('source_path', type=str, default=self.source_path, show_default=True)
         self.is_destination_ftp = click.prompt('is_destination_ftp', type=bool, default=self.is_destination_ftp, show_default=True)
         self.destination_path = click.prompt('destination_path', type=str, default=self.is_destination_ftp, show_default=True)
+        self.is_force = click.prompt('is_force', type=bool, default=self.is_force, show_default=True)
         self.is_delete = click.prompt('is_delete', type=bool, default=self.is_delete, show_default=True)
         self.is_dual = click.prompt('is_dual', type=bool, default=self.is_dual, show_default=True)
         ignore_regex_list = click.prompt('ignore_regex_list', type=list, default=[
@@ -140,7 +145,7 @@ class Configs:
         -------
         Configs
         '''
-        return Configs(configs_dict['order'], configs_dict['is_source_ftp'], configs_dict['source_path'], configs_dict['is_destination_ftp'], configs_dict['destination_path'], configs_dict['is_delete'], configs_dict['is_dual'], configs_dict['ignore_regex_list'], configs_dict['rename_dict'])
+        return Configs(configs_dict['order'], configs_dict['is_source_ftp'], configs_dict['source_path'], configs_dict['is_destination_ftp'], configs_dict['destination_path'], configs_dict['is_force'], configs_dict['is_delete'], configs_dict['is_dual'], configs_dict['ignore_regex_list'], configs_dict['rename_dict'])
 
 
 
@@ -158,6 +163,7 @@ class Configs:
             'source_path': self.source_path,
             'is_destination_ftp': self.is_destination_ftp,
             'destination_path': self.destination_path,
+            'is_force': self.is_force,
             'is_delete': self.is_delete,
             'is_dual': self.is_dual,
             'ignore_regex_list': [
